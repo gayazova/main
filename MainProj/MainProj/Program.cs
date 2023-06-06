134a4d8be743ffd8c58515fad7c404f0c9f3467c
Aliya.Gayazova <a.gaiazova@bars.group>
2023-06-06 13:14:15 +0300

    * ссылки

--- MainProj/MainProj/Program.cs
+++ MainProj/MainProj/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using StringExtensions;
+
 Console.WriteLine("Hello, World!");
+
+var value = StringExtension.GetValue(string.Empty);
+Console.WriteLine(value);
